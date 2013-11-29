5122787c8063c17058c7dd4c7965a7b75b7977f1
wplaat <wplaat@c561187e-80b4-ba2a-8bb7-eb1fdc67cd8c>
2013-11-29 22:05:04 +0000

    Rebalance defence moon buildings

--- trunk/php/#database/patch_60.sql
+++ trunk/php/#database/patch_60.sql
@@ -5,7 +5,7 @@ ALTER TABLE `statistics` CHANGE `ads` `gold` INT( 11 ) NOT NULL;
 UPDATE `statistics` SET gold = 0;
 
 
--- Earth Moon
+-- Earth Moon Units
 UPDATE `warquest`.`unit` SET `defense` = '410' WHERE `unit`.`uid` =127;
 UPDATE `warquest`.`unit` SET `defense` = '460' WHERE `unit`.`uid` =128;
 UPDATE `warquest`.`unit` SET `defense` = '625' WHERE `unit`.`uid` =228;
@@ -15,7 +15,7 @@ UPDATE `warquest`.`unit` SET `defense` = '595' WHERE `unit`.`uid` =327;
 UPDATE `warquest`.`unit` SET `defense` = '440' WHERE `unit`.`uid` =512;
 UPDATE `warquest`.`unit` SET `defense` = '490' WHERE `unit`.`uid` =513;
 
--- Rebalance Moon
+-- Rebalance Moon Units
 UPDATE `warquest`.`unit` SET `defense` = '2400' WHERE `unit`.`uid` =1209;
 UPDATE `warquest`.`unit` SET `defense` = '2000' WHERE `unit`.`uid` =1210;
 UPDATE `warquest`.`unit` SET `defense` = '2300' WHERE `unit`.`uid` =1211;
@@ -25,13 +25,37 @@ UPDATE `warquest`.`unit` SET `defense` = '150' WHERE `unit`.`uid` =810;
 UPDATE `warquest`.`unit` SET `defense` = '175' WHERE `unit`.`uid` =808;
 UPDATE `warquest`.`unit` SET `upkeep` = '1000000' WHERE `unit`.`uid` =917;
 
--- Rebalance Mars
+-- Rebalance Mars Units
 UPDATE `warquest`.`unit` SET `defense` = '650' WHERE `unit`.`uid` =1308;
 UPDATE `warquest`.`unit` SET `defense` = '1550' WHERE `unit`.`uid` =1409;
 UPDATE `warquest`.`unit` SET `defense` = '1600' WHERE `unit`.`uid` =1609;
 UPDATE `warquest`.`unit` SET `defense` = '1800' WHERE `unit`.`uid` =1607;
 
--- Rebalance Asteroid
+-- Rebalance Asteroid Units
 UPDATE `warquest`.`unit` SET `defense` = '1350' WHERE `unit`.`uid` =2009;
 UPDATE `warquest`.`unit` SET `defense` = '1550' WHERE `unit`.`uid` =2209;
 UPDATE `warquest`.`unit` SET `defense` = '1800' WHERE `unit`.`uid` =2207;
+
+-- Rebalance Moon Buildings
+UPDATE `warquest`.`building` SET `defense` = '250' WHERE `building`.`bid` =701;
+UPDATE `warquest`.`building` SET `defense` = '300' WHERE `building`.`bid` =704;
+UPDATE `warquest`.`building` SET `defense` = '450' WHERE `building`.`bid` =707;
+UPDATE `warquest`.`building` SET `defense` = '550' WHERE `building`.`bid` =710;
+UPDATE `warquest`.`building` SET `defense` = '650' WHERE `building`.`bid` =717;
+
+UPDATE `warquest`.`building` SET `defense` = '100' WHERE `building`.`bid` =700;
+UPDATE `warquest`.`building` SET `defense` = '250' WHERE `building`.`bid` =707;
+UPDATE `warquest`.`building` SET `defense` = '350' WHERE `building`.`bid` =708;
+UPDATE `warquest`.`building` SET `defense` = '450' WHERE `building`.`bid` =709;
+UPDATE `warquest`.`building` SET `defense` = '550' WHERE `building`.`bid` =716;
+
+UPDATE `warquest`.`building` SET `defense` = '100' WHERE `building`.`bid` =702;
+UPDATE `warquest`.`building` SET `defense` = '250' WHERE `building`.`bid` =705;
+UPDATE `warquest`.`building` SET `defense` = '355' WHERE `building`.`bid` =708;
+UPDATE `warquest`.`building` SET `defense` = '455' WHERE `building`.`bid` =718;
+
+UPDATE `warquest`.`building` SET `defense` = '550' WHERE `building`.`bid` =714;
+
+
+-- Rebalance Asteroid Buildings
+UPDATE `warquest`.`building` SET `defense` = '1000' WHERE `building`.`bid` =1701;
