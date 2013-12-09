10b63b99b519cce2f306cabd411a533790c0feec
wplaat <wplaat@c561187e-80b4-ba2a-8bb7-eb1fdc67cd8c>
2013-12-09 19:06:16 +0000

    Fix dynamic screen scale

--- trunk/php/#database/patch_61.sql
+++ trunk/php/#database/patch_61.sql
@@ -2,6 +2,14 @@
 -- WarQuest v6.1 data model patch
 --
 
+-- Remove old sun stuff
+DELETE FROM unit_group WHERE ugid>24;
+DELETE FROM building_group WHERE bgid>20;
+
+-- Remove players from sun, if any found
+UPDATE player SET planet=0 WHERE planet=4;
+
+-- Refactor planet numbers
 UPDATE building_group SET planet=4 WHERE planet=3;
 UPDATE building_group SET planet=3 WHERE planet=2;
 UPDATE building_group SET planet=2 WHERE planet=1;
@@ -17,9 +25,6 @@ UPDATE mission_group SET planet=3 WHERE planet=2;
 UPDATE mission_group SET planet=2 WHERE planet=1;
 UPDATE mission_group SET planet=1 WHERE planet=0;
 
--- Remove players from sun, if any found
-UPDATE player SET planet=0 WHERE planet=4;
-
 UPDATE player SET planet=4 WHERE planet=3;
 UPDATE player SET planet=3 WHERE planet=2;
 UPDATE player SET planet=2 WHERE planet=1;
@@ -34,4 +39,6 @@ UPDATE unit_group SET planet=4 WHERE planet=3;
 UPDATE unit_group SET planet=3 WHERE planet=2;
 UPDATE unit_group SET planet=2 WHERE planet=1;
 UPDATE unit_group SET planet=1 WHERE planet=0;
+
+
 
