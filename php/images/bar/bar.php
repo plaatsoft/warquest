f2aac5316ac4f05aff9a723c44771a9e6aa39906
wplaat <wplaat@c561187e-80b4-ba2a-8bb7-eb1fdc67cd8c>
2013-12-23 21:40:09 +0000

    Added more status bars

--- php/images/bar/bar.php
+++ php/images/bar/bar.php
@@ -13,19 +13,21 @@
 **  Or send an email to the following address.
 **  Email   : info@plaatsoft.nl
 **
-**  All copyrights reserved (c) 2008-2012 PlaatSoft
+**  All copyrights reserved (c) 2008-2013 PlaatSoft
 */
 
 /*
 ** Create all needed warquest progress bars. 
 */
-for ($value=0; $value<=100; $value++) {
+
+$offset=0;
+for ($value=0; $value<=300; $value++) {
 
 	$filename = 'bar'.$value.'.png';
 
 	echo 'Create '.$filename.'<br/>';
 	
-	$pos = floor((69 / 100) * $value);
+	$pos = floor((69 / 100) * ($value-($offset*100)));
 	
 	$im = imagecreate(71, 12);
 
@@ -36,9 +38,13 @@ for ($value=0; $value<=100; $value++) {
 	imagesetthickness($im, 2);	
 	imagerectangle($im, 0, 0, 71, 12, $white);		
 	imagefilledrectangle($im, 1, 1, $pos, 10, $orange);
-	imagestring($im, 2, 28, -1, $value.'%', $black);
+	imagestring($im, 2, 28, -1, ($value-($offset*100)).'%', $black);
 	
 	imagepng($im, $filename);
 
 	imagedestroy($im);
+	
+	if (($value>=100) && ($value%100)==0) {
+		$offset++;
+	}
 }
