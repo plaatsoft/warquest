da264964075e1da866e11ff767bd6916ec23728c
wplaat <wplaat@c561187e-80b4-ba2a-8bb7-eb1fdc67cd8c>
2014-03-03 17:33:15 +0000

    Add clan forum

--- trunk/server/php/#database/patch_66.sql
+++ trunk/server/php/#database/patch_66.sql
@@ -13,3 +13,6 @@ CREATE TABLE IF NOT EXISTS `views` (
 
 
 insert into views (mid, sid, ssid, eid, requests) values (0,0,0,0, 44926454);
+
+ALTER TABLE `player` ADD `clan_notification` INT NOT NULL AFTER `comment`;
+ALTER TABLE `comment` ADD `cid` INT NOT NULL AFTER `tid` ;
