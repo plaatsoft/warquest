3b372b0ca7adf7d32da0bd3ae0680ee7cfb81d2d
wplaat <wplaat@c561187e-80b4-ba2a-8bb7-eb1fdc67cd8c>
2014-03-10 05:12:18 +0000

    Added ajax news feed to home page

---
+++ trunk/server/php/js/news2.js
@@ -0,0 +1,51 @@
+/* All copyrights reserved (c) 2008-2014 PlaatSoft */
+
+var mid;
+var eid;
+
+function newsInit(mid, eid) {
+	this.mid = mid;
+	this.eid = eid;
+}
+
+function newsURL() {
+	var url = location.href;  
+	var baseURL = url.substring(0, url.indexOf('/', 14));
+
+	if (baseURL.indexOf('http://127.0.0.1') != -1) {
+
+		var url = location.href;  
+		var pathname = location.pathname;  
+		var index1 = url.indexOf(pathname);
+		var index2 = url.indexOf("/", index1 + 1);
+		var baseLocalUrl = url.substr(0, index2);
+	
+		return baseLocalUrl + "/";
+		
+	} else {
+
+		return baseURL + "/";
+	}
+}
+
+function newsCall() {
+
+	if (window.XMLHttpRequest) {
+		// code for IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp=new XMLHttpRequest();
+	} else {
+		// code for IE6, IE5
+		xmlhttp=new ActiveXObject('Microsoft.XMLHTTP');
+	}
+	
+	xmlhttp.onreadystatechange=function() {
+		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+		
+			document.getElementById("breakingnews").innerHTML += xmlhttp.responseText;
+		}
+	}
+	
+	xmlhttp.open('POST',  newsURL() ,true);
+	xmlhttp.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+	xmlhttp.send('mid='+mid+'&sid=0&eid='+eid);
+}
